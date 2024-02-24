b4a4b26b09e6a3a17bb7bca24551db878efadf10
Giovanni Orozco <gioxorozco@gmail.com>
2024-02-24 03:41:31 -0800

    Legacy push

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-var http = require('http');
-var fs = require('fs');
-var THREE = require('three');
+const http = require('http');
 
-http.createServer(function (req, res) {
-fs.readFile('index.html', function(err, data) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(data);
-  return res.end();
-  });
-}).listen(8080);
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http:);//${hostname}:${port}/`);
+});
